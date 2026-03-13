127cd2ec4ac21314a6db8062371d25c16918eb72
kcchanuk <kccantab@gmail.com>
2026-03-13 15:52:38 +0000

    Update Vacancy model

--- app/Models/Vacancy.php
+++ app/Models/Vacancy.php
@@ -7,5 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Vacancy extends Model
 {
     // Mass assignment
-    protected $fillable = ['job_title', 'location', 'short_desc', 'long_desc', 'min_salary', 'max_salary'];
+    protected $fillable = [
+        'job_title',
+        'location',
+        'short_desc',
+        'long_desc',
+        'min_salary',
+        'max_salary'
+    ];
 }
