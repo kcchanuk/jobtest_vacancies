fc138873ff0d2e347084763e06ca5d78af927daf
kcchanuk <kccantab@gmail.com>
2026-03-13 15:45:57 +0000

    Update migration file

--- database/migrations/2021_10_08_000000_create_vacancies_table.php
+++ database/migrations/2021_10_08_000000_create_vacancies_table.php
@@ -1,39 +1,34 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-class CreateVacanciesTable extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('vacancies', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->timestamps();
-            $table->softDeletes();
-
             $table->string('job_title');
             $table->string('location');
             $table->text('short_desc'); // Short Description
             $table->text('long_desc'); // Long Description
             $table->unsignedInteger('min_salary');
             $table->unsignedInteger('max_salary');
+
+            $table->timestamps();
+            $table->softDeletes();
         });
     }
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('vacancies');
     }
-}
+};
