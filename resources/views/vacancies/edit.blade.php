e73d1376554dcd67e9a29c246947ba6d5e4b5913
kcchanuk <kccantab@gmail.com>
2026-03-15 19:03:41 +0000

    Update edit blade template

--- resources/views/vacancies/edit.blade.php
+++ resources/views/vacancies/edit.blade.php
@@ -27,7 +27,7 @@
 
                 {{-- Form to delete a vacancy --}}
                 <form id="form_delete" method="POST" action="{{ route('vacancies.destroy', $vacancy) }}"
-                      accept-charset="UTF-8" class="mt-4" @submit="confirmDelete">
+                      accept-charset="UTF-8" class="mt-4" onsubmit="return confirm('{{ __('general.confirm_delete_vacancy') }}');">
                     @method('DELETE')
                     @csrf
                     <button type="submit" class="btn btn-danger">{{ __('general.delete_vacancy') }}</button>
@@ -36,23 +36,3 @@
         </div>
     </div>
 @endsection
-
-
-@push('scripts')
-    {{-- The user needs to confirm before deleting a vacancy --}}
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.14/vue.min.js"
-            integrity="sha512-XdUZ5nrNkVySQBnnM5vzDqHai823Spoq1W3pJoQwomQja+o4Nw0Ew1ppxo5bhF2vMug6sfibhKWcNJsG8Vj9tg=="
-            crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script>
-        new Vue({
-            el: '#form_delete',
-            methods: {
-                confirmDelete: function (e) {
-                    if (!confirm('{{ __('general.confirm_delete_vacancy') }}')) {
-                        e.preventDefault();
-                    }
-                }
-            }
-        });
-    </script>
-@endpush
